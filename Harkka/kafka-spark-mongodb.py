a4b579a1cf959774d7fbc5f4f352a3d83ca831ee
SaLuX8 <54550781+SaLuX8@users.noreply.github.com>
2020-10-31 23:22:42 +0200

    add images

--- Harkka/kafka-spark-mongodb.py
+++ Harkka/kafka-spark-mongodb.py
@@ -21,10 +21,10 @@ if __name__ == "__main__":
     brokers, topic = sys.argv[1:]
     kvs = KafkaUtils.createDirectStream(ssc, [topic],{"metadata.broker.list": brokers})
     lines = kvs.map(lambda x: json.loads(x[1]))
-    sensors = lines.map(lambda y : "Aika: " + str(y['weather'][0]['time']) + " | " + "Lämpötila (C):  " + str(y['weather'][0]['temp']) + " | " + "Tuulen nopeus (m/s):  " + str(y['weather'][0$
+    sensors = lines.map(lambda y : "Aika: " + str(y['weather'][0]['time']) + " | " + "Lämpötila (C):  " + str(y['weather'][0]['temp']) + " | " + "Tuulen nopeus (m/s):  " + str(y['weather'][0]['windspeed']))
     sensors.foreachRDD(lambda x: x.foreach(lambda y: print(y)))
     sensors.pprint()
-    rdd = lines.map(lambda x : {"timestamp": datetime.now(),"measuretime": x['weather'][0]['time'], "temp": x['weather'][0]['temp'], "windspeed": x['weather'][0]['windspeed'], "visibility": $
+    rdd = lines.map(lambda x : {"timestamp": datetime.now(),"measuretime": x['weather'][0]['time'], "temp": x['weather'][0]['temp'], "windspeed": x['weather'][0]['windspeed'], "visibility": x['weather'][0]['visibility']})
     rdd.foreachRDD(lambda z: saveAs(z))
     ssc.start()
     ssc.awaitTermination()
